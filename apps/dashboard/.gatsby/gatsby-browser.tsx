0d05d93abcef7e05b6a45081432ebb6db5498d74
kdichev <konstantin.dichev@gmail.com>
2020-10-01 20:27:55 +0000

    add typecheck

--- apps/dashboard/.gatsby/gatsby-browser.tsx
+++ apps/dashboard/.gatsby/gatsby-browser.tsx
@@ -1,1 +1,14 @@
-export { wrapRootElement } from "./../src/gatsby/wrapRootElement"
+import { GatsbyBrowser } from "gatsby"
+import React from "react"
+
+export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
+  element,
+}) => {
+  console.log("wrapRootElement Browser")
+  return (
+    <div>
+      <h1>title</h1>
+      {element}
+    </div>
+  )
+}
--- apps/dashboard/.gatsby/gatsby-ssr.tsx
+++ apps/dashboard/.gatsby/gatsby-ssr.tsx
@@ -1,1 +1,14 @@
-export { wrapRootElement } from "./../src/gatsby/wrapRootElement"
+import React from "react"
+import { GatsbySSR } from "gatsby"
+
+export const wrapRootElement: GatsbySSR["wrapRootElement"] = async ({
+  element,
+}) => {
+  console.log("wrapRootElement SSR")
+  return (
+    <div>
+      <h1>title</h1>
+      {element}
+    </div>
+  )
+}
